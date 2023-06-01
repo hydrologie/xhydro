46f640d959b7ff2e6ff8c1b023925c69d0060a89
TC-FF <tcff_hydro@outlook.com>
2023-06-01 15:35:28 -0400

    seasons related functions

--- xhydro/frequency_analysis/local.py
+++ xhydro/frequency_analysis/local.py
@@ -1,5 +1,6 @@
 import xarray as xr
 import numpy as np
+import warnings
 import copy
 
 class Data:
@@ -115,3 +116,116 @@ class Data:
         else:
         # +1 to include the end
             return self.data.sel(time=np.isin(self.data.time.dt.dayofyear, range(beg, end + 1))).groupby('time.year')
+        
+
+    @property
+    def season(self):
+        return self._season
+  
+    @season.setter
+    def season(self, 
+             liste: list):
+        """
+        The setter for the season property Issues a Warining if a new season is overlapping with another one. 
+        Alos issues a warning if the season name was already used, mand then overwrites it.
+        
+        Parameters
+        ----------
+        liste : List
+        List of Name, begining in Julian day, end in Julian day
+
+        Returns
+        -------
+        ds : xarray.DataSet
+        appends Dict self._season with name as key, and begining and end as values
+        
+        Examples
+        --------
+        >>> import xarray as xr
+        >>> cehq_data_path = '/dbfs/mnt/devdlzxxkp01/datasets/xhydro/tests/cehq/zarr'
+        >>> ds = xr.open_zarr(cehq_data_path, consolidated=True)
+        >>> donnees = Data(ds)
+        >>> donnees.season = ['Yearly', 1, 365]
+        """
+
+        #TODO a dictionary would probalby be more suited
+        name = liste[0]
+        beg = liste[1]
+        end = liste[2]
+        for season, dates in self.season.items():
+            if not isinstance(dates, xr.Dataset):
+                # We dont check for overlapping if season is a dataset
+                if name == season:
+                    warnings.warn('Warning, ' + name + ' was already defined and has been overwritten')
+                elif dates[0] <= beg and dates[1] >= beg or dates[0] <= end and dates[1] >= end:
+                    warnings.warn('Warning, ' + name + ' overlapping with ' + season)
+            
+        self._season[name] = [beg, end]
+
+
+    def rm_season(self, 
+                    name: str):
+        """
+        Fonction to remove a season. Isues a Warining if the name is not a valid season. 
+            
+        Parameters
+        ----------
+        name : Str
+        Name of the season to be removed
+
+        Returns
+        -------
+        ds : xarray.DataSet
+        The dataset is returned with =out the season specified. 
+        
+        Examples
+        --------
+        >>> import xarray as xr
+        >>> cehq_data_path = '/dbfs/mnt/devdlzxxkp01/datasets/xhydro/tests/cehq/zarr'
+        >>> ds = xr.open_zarr(cehq_data_path, consolidated=True)
+        >>> donnees = Data(ds)
+        >>> donnees.season = ['Yearly', 1, 365]
+        >>> donnees.rm_season('Yearly')
+        """
+
+        try:
+            del self._season[name]
+        except:
+            print('No season named ' + name)
+        
+    def get_seasons(self):
+        """
+        Function to list the seasons.
+        
+        Returns
+        -------
+        list : List
+        a list of the season names 
+        
+        Examples
+        --------
+        >>> import xarray as xr
+        >>> cehq_data_path = '/dbfs/mnt/devdlzxxkp01/datasets/xhydro/tests/cehq/zarr'
+        >>> ds = xr.open_zarr(cehq_data_path, consolidated=True)
+        >>> donnees = Data(ds)
+        >>> donnees.season = ['Yearly', 1, 365]
+        >>> donnees.season = ['Spring', 60, 120]
+        >>> ['Yearly', 'Spring']
+        """
+        return list(self.season.keys())
+    
+    def _get_season_values(self, 
+                            season: str):
+        """Function to get the values of a given season
+
+        Parameters
+        ----------
+        season : str
+            name of a previously defined season
+
+        Returns
+        -------
+        list
+            return a list of julian day of begining and end of season
+        """
+        return self._season[season]
