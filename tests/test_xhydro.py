18bc2134ab3e26329433986194b570f815e2f72e
Zeitsperre <10819524+Zeitsperre@users.noreply.github.com>
2023-06-09 17:03:42 -0400

    update setup.cfg bumpversion for black formatting, add a test for metadata

--- tests/test_xhydro.py
+++ tests/test_xhydro.py
@@ -2,6 +2,8 @@
 
 """Tests for `xhydro` package."""
 
+from pathlib import Path
+
 import pytest
 
 from xhydro import xhydro
@@ -15,9 +17,21 @@ def response():
     """
     # import requests
     # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    pass
 
 
 def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    pass
+
+
+def test_imports():
+    metadata = Path(xhydro.__file__).resolve().parent.joinpath("__init__.py")
+
+    with open(metadata) as f:
+        contents = f.read()
+        assert '__author__ = """Thomas-Charles Fortier Filion"""' in contents
+        assert '__email__ = "tcff_hydro@outlook.com"' in contents
+        assert '__version__ = "0.1.3"' in contents
