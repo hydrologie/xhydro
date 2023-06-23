40e74ad639aa3d8333ab4d3d6bb7b162f1441744
TC-FF <tcff_hydro@outlook.com>
2023-06-23 11:53:12 -0400

    updated init to run first Notebook

--- xhydro/__init__.py
+++ xhydro/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """Thomas-Charles Fortier Filion"""
 __email__ = "tcff_hydro@outlook.com"
 __version__ = "0.1.5"
+
+from .frequency_analysis.local import *
+from .utils import get_julian_day, get_timestep
