160d54488821f379e33646692de6316404e2e5a7
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2023-07-21 14:51:43 -0400

    first working version

---
+++ xhydro/modelling/_hydrotel.py
@@ -0,0 +1,230 @@
+import os
+import re
+import yaml
+from pathlib import Path, PureWindowsPath
+import subprocess
+from copy import deepcopy
+
+import pandas as pd
+import xarray as xr
+import xclim as xc
+
+from .data_checks import health_checks
+
+
+class Hydrotel:
+    def __init__(self,
+                 project: str | os.PathLike,
+                 *,
+                 default_options: bool = True,
+                 simulation_options: dict = None,
+                 output_options: dict = None,
+                 ):
+        """
+        Class to handle Hydrotel simulations.
+
+        Parameters
+        ----------
+        project: str | os.PathLike
+            Path to the project folder.
+        """
+
+        self.project = Path(project)
+
+        if default_options is False and (not os.path.isfile(self.project / 'simulation' / 'simulation' / 'simulation.csv') or
+                                         not os.path.isfile(self.project / 'simulation' / 'simulation' / 'output.csv')):
+            raise ValueError("The simulation and output files must exist if default_options=False.")
+
+        # Load from the files, if they exist. Otherwise use default values
+        if os.path.isfile(self.project / 'simulation' / 'simulation' / 'simulation.csv') and default_options is False:
+            self.simulation_options = pd.read_csv(self.project / 'simulation' / 'simulation' / 'simulation.csv',
+                                                  delimiter=";", header=None, index_col=0).squeeze().to_dict()
+        else:
+            with open(Path(__file__).parent / "hydrotel_defaults.yml") as f:
+                self.simulation_options = yaml.safe_load(f)["simulation_options"]
+        self.simulation_options = _fix_dates(_fix_os_paths(self.simulation_options))
+
+        if os.path.isfile(self.project / 'simulation' / 'simulation' / 'output.csv') and default_options is False:
+            self.output_options = pd.read_csv(self.project / 'simulation' / 'simulation' / 'output.csv', delimiter=";", header=None, index_col=0).squeeze().to_dict()
+        else:
+            with open(Path(__file__).parent / "hydrotel_defaults.yml") as f:
+                self.output_options = yaml.safe_load(f)["output_options"]
+
+        # Update options with the user-provided ones
+        self.update(simulation_options=simulation_options, output_options=output_options)
+
+        # TODO: Clean up and prepare the 'etat' folder (missing the files)
+
+    def update(self,
+               *,
+               simulation_options: dict = None,
+               output_options: dict = None):
+        """
+        Updates the simulation and output files.
+
+        Parameters
+        ----------
+        simulation_options: dict
+            Dictionary of options to change in the simulation file.
+        output_options: dict
+            Dictionary of options to change in the output file.
+        """
+        if simulation_options is not None:
+            # Update options with the user-provided ones
+            simulation_options = deepcopy(_fix_dates(_fix_os_paths(simulation_options))) or {}
+            for key, value in simulation_options.items():
+                self.simulation_options[key] = value
+
+            # Save the simulation options to a file
+            df = pd.DataFrame.from_dict(self.simulation_options, orient='index')
+            df = df.replace({None: ''})
+            df.to_csv(self.project / 'simulation' / 'simulation' / 'simulation.csv', sep=';', header=False, columns=[0])
+
+        if output_options is not None:
+            output_options = deepcopy(output_options) or {}
+            for key, value in output_options.items():
+                self.output_options[key] = value
+
+            # Save the output options to a file
+            df = pd.DataFrame.from_dict(output_options, orient='index')
+            df.to_csv(self.project / 'simulation' / 'simulation' / 'output.csv', sep=';', header=False)
+
+    def basic_checkups(self):
+        # Make sure that all the files exist
+        for key, value in self.simulation_options.items():
+            if re.match("^.[a-z]", Path(str(value)).suffix):
+                if Path(value).is_absolute() == False:
+                    # FIXME: Why are the paths written this way?
+                    if str(Path(value).parent) != ".":
+                        value = self.project / value
+                    else:
+                        value = self.project / 'simulation' / 'simulation' / value
+                if not Path(value).is_file():
+                    raise FileNotFoundError(f'The file {value} does not exist.')
+
+        # Make sure that the start and end dates of the simulation are contained in the meteo file
+        start_date = pd.to_datetime(self.simulation_options.get('DATE DEBUT', None))
+        end_date = pd.to_datetime(self.simulation_options.get('DATE FIN', None))
+        weather_file = self.project / self.simulation_options.get('FICHIER STATIONS METEO', None)
+        ds = xr.open_dataset(weather_file, cache=False)
+        if not ((ds.time[0] <= start_date) and (ds.time[-1] >= end_date)).item():
+            raise ValueError(f'The start date ({start_date}) or end date ({end_date}) are outside the bounds of the weather file ({weather_file}).')
+
+    def advanced_checkups(self, xr_open_kwargs: dict = None, **kwargs):
+        structure = {"dims": ["time", "stations"], "coords": ['lat', 'lon', 'x', 'y', 'z']}
+        variables_and_units = {"tasmin": "degC", "tasmax": "degC", "pr": "mm"}
+
+        ds = xr.open_dataset(self.project / self.simulation_options['FICHIER STATIONS METEO'], **(xr_open_kwargs or {}))
+        health_checks(ds, structure=structure, calendar="standard", variables_and_units=variables_and_units, **kwargs)
+
+    def run(self,
+            *,
+            hydrotel_console: str | os.PathLike = None,
+            id_as_dim: bool = True,
+            return_output: bool = False,
+            xr_open_kwargs: dict = None):
+        """
+        Run the simulation.
+
+        Parameters
+        ----------
+        hydrotel_console: str | os.PathLike
+            On Windows, path to Hydrotel.exe.
+
+        """
+        self.basic_checkups()
+
+        if os.name == 'nt':  # Windows
+            if hydrotel_console is None:
+                raise ValueError('You must specify the path to Hydrotel.exe')
+        else:
+            hydrotel_console = 'hydrotel'
+
+        # TODO: Test it out
+        # subprocess.check_call(hydrotel_console + ' ' + str(self.project) + ' -t 1')
+
+        # Standardize the outputs
+        ds = self._standardize_outputs(id_as_dim=id_as_dim, xr_open_kwargs=xr_open_kwargs)
+
+        if return_output:
+            return ds
+        else:
+            # TODO: Save instead of returning
+            raise NotImplementedError
+
+    def _standardize_outputs(self,
+                             *,
+                             id_as_dim: bool = True,
+                             xr_open_kwargs: dict = None):
+        """ Standardize the outputs of the simulation. """
+        ds = self.get_streamflow(**(xr_open_kwargs or {}))
+
+        # Rename variables to standard names
+        ds = ds.assign_coords(troncon=ds["troncon"], idtroncon=ds["idtroncon"])
+        ds = ds.rename({'troncon': 'station', 'idtroncon': 'station_id', 'debit_aval': 'streamflow'})
+        if id_as_dim:
+            ds = ds.swap_dims({'station': 'station_id'})
+        ds = ds.drop_vars(['station'])
+        ds['station_id' if id_as_dim else 'station'].attrs["cf_role"] = "timeseries_id"
+        # FIXME: Do we always want station_id as the dimension?
+
+        # Add standard attributes for streamflow and fix units
+        ds['streamflow'].attrs['original_name'] = 'debit_aval'
+        for attr in ['standard_name', 'long_name', 'description', 'units']:
+            if attr in ds['streamflow'].attrs:
+                ds['streamflow'].attrs[f"original_{attr}"] = ds['streamflow'].attrs[attr]
+        ds['streamflow'].attrs['standard_name'] = 'outgoing_water_volume_transport_along_river_channel'
+        ds['streamflow'].attrs['long_name'] = 'Streamflow'
+        ds['streamflow'].attrs['description'] = 'Streamflow at the outlet of the river reach'
+        ds['streamflow'] = xc.units.convert_units_to(ds['streamflow'], 'm3 s-1')
+
+        # Add standard attributes for SWE and fix units
+        # TODO: (currently missing the file)
+
+        return ds
+
+    def get_streamflow(self, **kwargs):
+        """ Get the streamflow from the simulation.
+
+        Returns
+        -------
+        ds: xr.Dataset
+            Dataset containing the streamflow.
+        """
+        # FIXME: We temporarily work with a clean copy of the output, since we can't run Hydrotel
+        import shutil
+        if os.path.isfile(os.path.join(self.project, 'simulation', 'simulation', 'resultat', 'debit_aval.nc')):
+            os.remove(os.path.join(self.project, 'simulation', 'simulation', 'resultat', 'debit_aval.nc'))
+        shutil.copy(os.path.join(self.project, 'simulation', 'simulation', 'resultat', 'debit_aval_orig.nc'),
+                    os.path.join(self.project, 'simulation', 'simulation', 'resultat', 'debit_aval.nc'))
+
+        return xr.open_dataset(self.project / 'simulation' / 'simulation' / 'resultat' / 'debit_aval.nc', **kwargs)
+
+
+def _fix_os_paths(d: dict):
+    """ Convert paths to fit the OS.
+    """
+    # FIXME: Ugly fix to switch Windows paths to the right OS. Wouldn't work otherwise.
+    return {k: str(Path(PureWindowsPath(v).as_posix())) if any(slash in str(v) for slash in ['/', '\\']) else v for k, v in d.items()}
+
+
+def _fix_dates(d: dict):
+    """ Convert dates to the right format.
+    """
+    # Reformat dates
+    for key in ['DATE DEBUT', 'DATE FIN']:
+        if key in d and not pd.isnull(d[key]):
+            d[key] = pd.to_datetime(d[key]).strftime('%Y-%m-%d %H:%M')
+    # FIXME: Validate that this part is useful like this
+    for key in ['LECTURE ETAT FONTE NEIGE', 'LECTURE ETAT TEMPERATURE DU SOL', 'LECTURE ETAT BILAN VERTICAL',
+                'LECTURE ETAT RUISSELEMENT SURFACE', 'LECTURE ETAT ACHEMINEMENT RIVIERE']:
+        if key in d and not pd.isnull(d[key]):
+            d[key] = d[key].replace(Path(d[key]).stem.split("_")[-1],
+                                    pd.to_datetime(Path(d[key]).stem.split("_")[-1]).strftime('%Y%m%d%H'))
+    for key in ['ECRITURE ETAT FONTE NEIGE', 'ECRITURE ETAT TEMPERATURE DU SOL', 'ECRITURE ETAT BILAN VERTICAL',
+                'ECRITURE ETAT RUISSELEMENT SURFACE', 'ECRITURE ETAT ACHEMINEMENT RIVIERE']:
+        if key in d and not pd.isnull(d[key]):
+            # FIXME: Do we realy need to get rid of the minutes?
+            d[key] = pd.to_datetime(d[key]).strftime('%Y-%m-%d %H')
+
+    return d
--- xhydro/modelling/data_checks.py
+++ xhydro/modelling/data_checks.py
@@ -2,52 +2,82 @@ import xclim as xc
 import xclim.core.dataflags
 import xarray as xr
 
+import warnings
 
-# FIXME: Make this function more generic, missing is not working
-def health_check(ds,
-                 model: str,
+
+def health_checks(ds,
                  *,
+                 structure: dict = None,
                  calendar: str = None,
+                 variables_and_units: dict = None,
                  missing: str = None,
                  flags: dict = None,
                  raise_flags: bool = True):
 
     # Check the dimensions and coordinates
-    if model == 'hydrotel':
-        if "stations" not in ds.dims:
-            raise ValueError("The dimension 'stations' is missing.")
-        # TODO: What is important in lat/lon/x/y/z ? Do we raise an error if they are missing?
-        for coord in ['lat', 'lon', 'x', 'y', 'z']:
-            if coord not in ds.coords and coord in ds:
-                ds = ds.assign_coords({coord: ds[coord]})
-
-    else:
-        raise NotImplementedError(f'The model {model} is not implemented.')
+    if structure is not None:
+        for dim in structure["dims"]:
+            if dim not in ds.dims:
+                raise ValueError(f"The dimension {dim} is missing.")
+        for coord in structure["coords"]:
+            if coord not in ds.coords:
+                if coord in ds.data_vars:
+                    warnings.warn(f"The variable {coord} is detected as a data variable, not a coordinate.", UserWarning, stacklevel=1)
+                else:
+                    raise ValueError(f"The coordinate {coord} is missing.")
 
     # Check the calendar
     if calendar is not None:
-        # TODO: Support calendars with multiple names (standard, gregorian, proleptic_gregorian)
-        if ds.time.encoding['calendar'] != calendar:
+        trans = {
+            "proleptic_gregorian": "standard",
+            "gregorian": "standard",
+            "default": "standard",
+            "366_day": "all_leap",
+            "365_day": "noleap",
+            "julian": "standard",
+        }
+        cal = xc.core.calendar.get_calendar(ds.time)
+        if not any(c in [cal, trans[cal]] for c in [calendar, trans[calendar] if calendar in trans else None]):
             raise ValueError(f"The calendar is not {calendar}.")
+
+    # Check variables
+    if variables_and_units is not None:
+        for v in variables_and_units:
+            if v not in ds:
+                raise ValueError(f"The variable {v} is missing.")
+            xc.core.units.check_units(ds[v], variables_and_units[v])
 
     # Quick check for irregular time steps
     freq = xr.infer_freq(ds.time)
     if freq is None:
         raise ValueError("The timesteps are irregular and cannot be inferred.")
 
-    # Check variables
-    if model == 'hydrotel':
-        variables = {'pr': 'mm', 'tasmin': 'degC', 'tasmax': 'degC'}
-    else:
-        raise NotImplementedError(f'The model {model} is not implemented.')
+    if missing is not None:
+        if freq not in ["Y", "YS", "M", "MS", "H"]:
+            warnings.warn(f"The frequency {freq} is not supported for missing data checks. That check will be skipped.", UserWarning, stacklevel=1)
+        else:
+            if isinstance(missing, str):
+                missing = {missing: {}}
+            if len(missing) > 1:
+                raise NotImplementedError("Only one missing check can be performed at a time.")
+            method = list(missing.keys())[0]
+            if "freq" not in missing[method]:
+                missing[method]["freq"] = "YS"
+            for v in ds.data_vars:
+                if missing == "any":
+                    ms = xc.core.missing.missing_any(ds[v], **missing["any"])
+                elif missing == "at_least_n_valid":
+                    ms = xc.core.missing.at_least_n_valid(ds[v], **missing["at_least_n_valid"])
+                elif missing == "missing_pct":
+                    ms = xc.core.missing.missing_pct(ds[v], **missing["missing_pct"])
+                elif missing == "wmo":
+                    ms = xc.core.missing.missing_wmo(ds[v], **missing["wmo"])
+                else:
+                    raise ValueError(f"Missing check {missing} is not implemented.")
+                if ms.any():
+                    warnings.warn(f"The variable '{v}' has missing values.", UserWarning, stacklevel=1)
 
-    for v in variables:
-        if v not in ds:
-            raise ValueError(f"The variable {v} is missing.")
-        if ds[v].attrs['units'] != variables[v]:
-            ds[v] = xc.units.convert_units_to(ds[v], variables[v])
-
-        # xc.core.missing.missing_any(ds[v], '3h')
-
-        if v in flags:
-            xclim.core.dataflags.data_flags(ds[v], ds, flags=flags[v], raise_flags=raise_flags)
+    if flags is not None:
+        for v in ds.data_vars:
+            if v in flags:
+                xclim.core.dataflags.data_flags(ds[v], ds, flags=flags[v], raise_flags=raise_flags)
