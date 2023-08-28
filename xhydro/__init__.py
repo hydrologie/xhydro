dce507652c950e1d296c5d82d9c770d44c351f32
sebastienlanglois <sebastien.langlois@polymtl.ca>
2023-08-28 00:58:23 -0400

    make frequency_analysis a module

--- xhydro/__init__.py
+++ xhydro/__init__.py
@@ -4,5 +4,4 @@ __author__ = """Thomas-Charles Fortier Filion"""
 __email__ = "tcff_hydro@outlook.com"
 __version__ = "0.1.5"
 
-from .frequency_analysis.local import *
 from .utils import get_julian_day, get_timestep
