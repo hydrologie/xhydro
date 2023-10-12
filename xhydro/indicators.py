9a088c330bb4efb64b9432f644dd49c6b77c558f
TC-FF <tcff_hydro@outlook.com>
2023-10-12 14:17:38 -0400

    added mannkendall functions from pymannkendall

--- xhydro/indicators.py
+++ xhydro/indicators.py
@@ -4,6 +4,8 @@ import warnings
 import xarray as xr
 import xclim as xc
 import xscen as xs
+import pymannkendall
+import numpy as np
 from xclim.core.units import rate2amount
 
 # Special imports from xscen
@@ -239,3 +241,20 @@ def get_yearly_op(
     out = xs.clean_up(out, common_attrs_only=ind_dict)
 
     return out
+
+def _pval_mannkendall_ufunc(da, 
+                            test, 
+                            outputs):
+    da = da[~np.isnan(da)]
+    pmk = getattr(pymannkendall, test)
+    mk = pmk(da)
+    return tuple([getattr(mk, out) for out in outputs])
+
+def mannkendall(da,
+                     outputs: list = ['h', 'p'], 
+                     test: str ='original_test'):
+    try:
+        return xr.concat(xr.apply_ufunc(_pval_mannkendall_ufunc, da, test, outputs, input_core_dims=[['time'],[], ['dim0']],output_core_dims=np.empty((len(outputs), 0)).tolist(), vectorize=True) , dim='mk').assign_coords(mk=outputs)
+    except:
+        return xr.apply_ufunc(_pval_mannkendall_ufunc, da, test, outputs, input_core_dims=[['time'],[], ['dim0']],output_core_dims=np.empty((len(outputs), 0)).tolist(), output_dtypes=[tuple],vectorize=True).assign_coords(mk=outputs)
+
