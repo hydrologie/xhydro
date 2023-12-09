f46847e9e689e69c86c87d16e2c6345fbdd37d2b
richardarsenault <richard.arsenault@etsmtl.ca>
2023-12-09 00:05:15 -0500

    cleaned calibration

--- xhydro/calibration.py
+++ xhydro/calibration.py
@@ -12,6 +12,7 @@ from spotpy import analyser
 from spotpy.parameter import Uniform
 import spotpy
 import hydroeval as he
+from xhydro.hydrological_modelling import  hydrological_model_selector
 
 class spot_setup(object):
     
@@ -80,32 +81,7 @@ def perform_calibration(model_config, evaluation_metric, maximize, bounds_high, 
     #objfun = get_objective_function_value(model_config['Qobs'], Qsim, best_parameters, obj_func=evaluation_metric)
     
     return best_parameters, Qsim, bestobjf
-
-
-def dummy_model(model_config):    
-    
-    """
-    Dummy model to show the implementation we should be aiming for. Each model 
-    will have its own required data that users can pass. We could envision a setup
-    where a class generates the model_config object for all model formats making it 
-    possible to replace models on the fly
-    """
-    
-    precip = model_config['precip']
-    temperature = model_config['temperature']
-    drainage_area = model_config['drainage_area']
-    parameters = model_config['parameters']
-    
-    Q = np.empty(len(precip))
-    
-    for t in range(0,len(precip)):
-     
-        Q[t]= (precip[t] * parameters[0] + abs(temperature[t])*parameters[1])* parameters[2] * drainage_area
-        
-        
-    return Q
-
-        
+       
  
     
 def transform_flows(Qobs, Qsim, transform=None, epsilon=None):
@@ -137,17 +113,6 @@ def transform_flows(Qobs, Qsim, transform=None, epsilon=None):
         Qobs, Qsim = np.sqrt(Qobs), np.sqrt(Qsim)
     
     return Qobs, Qsim
-
-
-
-def hydrological_model_selector(model_config):
-
-    if model_config['model_name'] == 'Dummy':
-        Qsim = dummy_model(model_config)
-        
-        # ADD OTHER MODELS HERE
-
-    return Qsim
 
 
 def get_objective_function_value(Qobs, Qsim, params, transform=None, epsilon=None, obj_func=None):
