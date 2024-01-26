f378152333a3f19d6e6348a7119e2615cda247c3
Francis Gravel <fgr4vel@gmail.com>
2024-01-26 15:36:18 -0500

    Corrected parralel test

--- tests/optimal_interpolation/test_integration.py
+++ tests/optimal_interpolation/test_integration.py
@@ -71,18 +71,46 @@ def test_cross_validation_execute():
     assert len(result_flows[2]) == 365
 
 def test_cross_validation_execute_parralelize():
-    start_date = '2018-01-01'
-    end_date = '2019-01-01'
+
+
+    station_info_file = get_file(name="data/optimal_interpolation/Info_Station.csv",
+                        github_url="https://github.com/Mayetea/xhydro-testdata",
+                        branch="optimal_interpolation")
+    corresponding_station_file = get_file(name="data/optimal_interpolation/Correspondance_Station.csv",
+                                 github_url="https://github.com/Mayetea/xhydro-testdata",
+                                 branch="optimal_interpolation")
+    selected_station_file = get_file(name="data/optimal_interpolation/stations_retenues_validation_croisee.csv",
+                                 github_url="https://github.com/Mayetea/xhydro-testdata",
+                                 branch="optimal_interpolation")
+    flow_obs_info_file = get_file(name="data/optimal_interpolation/A20_HYDOBS_TEST.nc",
+                                 github_url="https://github.com/Mayetea/xhydro-testdata",
+                                 branch="optimal_interpolation")
+    flow_sim_info_file = get_file(name="data/optimal_interpolation/A20_HYDREP_TEST.nc",
+                                 github_url="https://github.com/Mayetea/xhydro-testdata",
+                                 branch="optimal_interpolation")
+
     files = [
-        constants.DATA_PATH + "Table_Info_Station_Hydro_2020.csv",
-        constants.DATA_PATH + "Table_Correspondance_Station_Troncon.csv",
-        constants.DATA_PATH + "stations_retenues_validation_croisee.csv",
-        constants.DATA_PATH + 'A20_HYDOBS.nc',
-        constants.DATA_PATH + 'A20_HYDREP.nc'
+        station_info_file,
+        corresponding_station_file,
+        selected_station_file,
+        flow_obs_info_file,
+        flow_sim_info_file
     ]
+
+    start_date = dt.datetime(2018, 11, 1)
+    end_date = dt.datetime(2019, 1, 1)
 
     result_flows = cv.execute(start_date, end_date, files)
 
     # Test some output flow values
+    assert result_flows[0][-1, 0] == 7.884402090442147
+    assert result_flows[0][-2, 0] == 8.170066622988958
+
+    # To randomize to test direct values
     assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
     assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
+
+    # Test the time range duration
+    assert len(result_flows[0]) == 365
+    assert len(result_flows[1]) == 365
+    assert len(result_flows[2]) == 365
