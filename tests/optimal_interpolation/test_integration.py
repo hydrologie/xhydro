c0b69eda79e83b03c549d26b6401a474e1278f1e
richardarsenault <richard.arsenault@etsmtl.ca>
2024-01-26 16:55:18 -0500

    starting to update tests

--- tests/optimal_interpolation/test_integration.py
+++ tests/optimal_interpolation/test_integration.py
@@ -1,116 +1,103 @@
-import xhydro.optimal_interpolation.cross_validation as cv
-from xhydro.optimal_interpolation import constants
-import numpy as np
-from pytest import approx
-from xhydro.optimal_interpolation.functions.testdata import get_file
 import datetime as dt
 
-def test_cross_validation_execute():
+import numpy as np
+from pytest import approx
+
+import xhydro.optimal_interpolation.cross_validation as cv
+from xhydro.optimal_interpolation.functions.testdata import get_file
 
 
-    station_info_file = get_file(name="data/optimal_interpolation/Info_Station.csv",
-                        github_url="https://github.com/Mayetea/xhydro-testdata",
-                        branch="optimal_interpolation")
-    corresponding_station_file = get_file(name="data/optimal_interpolation/Correspondance_Station.csv",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    selected_station_file = get_file(name="data/optimal_interpolation/stations_retenues_validation_croisee.csv",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    flow_obs_info_file = get_file(name="data/optimal_interpolation/A20_HYDOBS_TEST.nc",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    flow_sim_info_file = get_file(name="data/optimal_interpolation/A20_HYDREP_TEST.nc",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
+class test_optimal_interpolation_integration:
+    # Set Github URL for getting files for tests
+    git_url = "https://github.com/Mayetea/xhydro-testdata"
+    branch = "optimal_interpolation"
+    dataf = "data/optimal_interpolation/"
 
+    # Prepare files. Get them on the public data repo.
+    station_info_file = get_file(
+        name="Info_Station.csv", github_url=git_url, branch=branch
+    )
+    corresponding_station_file = get_file(
+        name=dataf + "Correspondance_Station.csv", github_url=git_url, branch=branch
+    )
+    selected_station_file = get_file(
+        name=dataf + "stations_retenues_validation_croisee.csv",
+        github_url=git_url,
+        branch=branch,
+    )
+    flow_obs_info_file = get_file(
+        name=dataf + "A20_HYDOBS_TEST.nc", github_url=git_url, branch=branch
+    )
+    flow_sim_info_file = get_file(
+        name=dataf + "A20_HYDREP_TEST.nc", github_url=git_url, branch=branch
+    )
+
+    # Make a list with these files paths, required for the code.
     files = [
         station_info_file,
         corresponding_station_file,
         selected_station_file,
         flow_obs_info_file,
-        flow_sim_info_file
+        flow_sim_info_file,
     ]
 
+    # Start and end dates for the simulation. Short period for the test.
     start_date = dt.datetime(2018, 11, 1)
     end_date = dt.datetime(2019, 1, 1)
 
-    result_flows = cv.execute(start_date, end_date, files, False)
+    def test_cross_validation_execute(self, files, start_date, end_date):
+        """test the cross validation of optimal interpolation."""
 
-    # Test some output flow values
-    assert result_flows[0][-1, 0] == 8.042503657491906
-    assert result_flows[0][-2, 0] == 8.377341430781929
+        # Run the code and obtain the resulting flows.
+        result_flows = cv.execute(start_date, end_date, files, parallelize=False)
 
-    # To randomize to test direct values
-    assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
-    assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
+        # Test some output flow values
+        assert result_flows[0][-1, 0] == 8.042503657491906
+        assert result_flows[0][-2, 0] == 8.377341430781929
 
-    # Test the time range duration
-    assert len(result_flows[0]) == 61
-    assert len(result_flows[1]) == 61
-    assert len(result_flows[2]) == 61
+        # To randomize to test direct values
+        assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
+        assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
 
-    # Test a different data range to verify that the last entry is different
+        # Test the time range duration
+        assert len(result_flows[0]) == 61
+        assert len(result_flows[1]) == 61
+        assert len(result_flows[2]) == 61
 
-    start_date = dt.datetime(2018, 1, 1)
-    end_date = dt.datetime(2019, 1, 1)
+        # Test a different data range to verify that the last entry is different
+        start_date = dt.datetime(2018, 1, 1)
+        end_date = dt.datetime(2018, 1, 31)
 
-    result_flows = cv.execute(start_date, end_date, files, False)
+        result_flows = cv.execute(start_date, end_date, files, parallelize=False)
 
-    # Test some output flow values
-    assert result_flows[0][-1, 0] == 7.884402090442147
-    assert result_flows[0][-2, 0] == 8.170066622988958
+        # Test some output flow values
+        assert result_flows[0][-1, 0] == 7.884402090442147
+        assert result_flows[0][-2, 0] == 8.170066622988958
 
-    # To randomize to test direct values
-    assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
-    assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
+        # To randomize to test direct values
+        assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
+        assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
 
-    # Test the time range duration
-    assert len(result_flows[0]) == 365
-    assert len(result_flows[1]) == 365
-    assert len(result_flows[2]) == 365
+        # Test the time range duration
+        assert len(result_flows[0]) == 31
+        assert len(result_flows[1]) == 31
+        assert len(result_flows[2]) == 31
 
-def test_cross_validation_execute_parralelize():
+    def test_cross_validation_execute_parralelize(self, files, start_date, end_date):
+        """Test the parallel version of the optimal interpolation cross validation."""
 
+        # Run the interpolation and get flows
+        result_flows = cv.execute(start_date, end_date, files)
 
-    station_info_file = get_file(name="data/optimal_interpolation/Info_Station.csv",
-                        github_url="https://github.com/Mayetea/xhydro-testdata",
-                        branch="optimal_interpolation")
-    corresponding_station_file = get_file(name="data/optimal_interpolation/Correspondance_Station.csv",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    selected_station_file = get_file(name="data/optimal_interpolation/stations_retenues_validation_croisee.csv",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    flow_obs_info_file = get_file(name="data/optimal_interpolation/A20_HYDOBS_TEST.nc",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
-    flow_sim_info_file = get_file(name="data/optimal_interpolation/A20_HYDREP_TEST.nc",
-                                 github_url="https://github.com/Mayetea/xhydro-testdata",
-                                 branch="optimal_interpolation")
+        # Test some output flow values
+        assert result_flows[0][-1, 0] == 7.884402090442147
+        assert result_flows[0][-2, 0] == 8.170066622988958
 
-    files = [
-        station_info_file,
-        corresponding_station_file,
-        selected_station_file,
-        flow_obs_info_file,
-        flow_sim_info_file
-    ]
+        # To randomize to test direct values
+        assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
+        assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
 
-    start_date = dt.datetime(2018, 11, 1)
-    end_date = dt.datetime(2019, 1, 1)
-
-    result_flows = cv.execute(start_date, end_date, files)
-
-    # Test some output flow values
-    assert result_flows[0][-1, 0] == 7.884402090442147
-    assert result_flows[0][-2, 0] == 8.170066622988958
-
-    # To randomize to test direct values
-    assert np.nanmean(result_flows[1][:, :]) == approx(33, 0.5)
-    assert np.nanmean(result_flows[2][:, :]) == approx(59, 0.5)
-
-    # Test the time range duration
-    assert len(result_flows[0]) == 365
-    assert len(result_flows[1]) == 365
-    assert len(result_flows[2]) == 365
+        # Test the time range duration
+        assert len(result_flows[0]) == 61
+        assert len(result_flows[1]) == 61
+        assert len(result_flows[2]) == 61
--- tests/optimal_interpolation/test_mathematical_algorithms.py
+++ tests/optimal_interpolation/test_mathematical_algorithms.py
@@ -1,10 +1,16 @@
-import xhydro.optimal_interpolation.functions.mathematical_algorithms as ma
 import numpy as np
 
+import xhydro.optimal_interpolation.functions.mathematical_algorithms as ma
+
+
 def test_calculate_average_distance():
+    """Test calculating average distance between points."""
+
+    # Between 2 points
     x_points = np.array([[0, 3]])
     y_points = np.array([[0, 4]])
 
+    # Calculate distance
     result = ma.calculate_average_distance(x_points, y_points)
 
     assert len(result) == 2
@@ -14,22 +20,25 @@ def test_calculate_average_distance():
     assert result[1, 0] == 5
     assert result[1, 1] == 0
 
-
+    # Test between 2 sets of vectors
     x_points = np.array([[0, 1], [0, 1]])
     y_points = np.array([[0, 1], [0, 1]])
 
-    result = ma.calculate_average_distance(x_points,y_points)
+    # Calculate distance
+    result = ma.calculate_average_distance(x_points, y_points)
 
     assert len(result) == 2
     assert len(result[0]) == 2
-    assert result[0,0] == 0
-    assert result[0,1] == 1.4142135623730951
-    assert result[1,0] == 1.4142135623730951
-    assert result[1,1] == 0
+    assert result[0, 0] == 0
+    assert result[0, 1] == 1.4142135623730951
+    assert result[1, 0] == 1.4142135623730951
+    assert result[1, 1] == 0
 
+    # Test between 2 sets of points in 3D
     x_points = np.array([[1, 2, 3], [4, 5, 6]])
     y_points = np.array([[1, 2, 3], [4, 5, 6]])
 
+    # Calculate distance
     result = ma.calculate_average_distance(x_points, y_points)
 
     assert len(result) == 3
@@ -46,18 +55,22 @@ def test_calculate_average_distance():
 
 
 def test_latlon_to_xy():
+    """Test conversion between latitude longitude and x-y coordinates."""
     lat = 10
     lon = -45
 
+    # Do the conversion
     result = ma.latlon_to_xy(lat, lon)
     assert result[0] == -4436.536575078841
     assert result[1] == 1106.312539916013
 
+    # Test using a reference
     lat = 0
     lon = 0
     lat0 = 10
     lon0 = -45
 
-    result = ma.latlon_to_xy(lat, lon, lat0, lon0)
+    # Do the conversion
+    result = ma.latlon_to_xy(lat, lon, lat0=lat0, lon0=lon0)
     assert result[0] == 4504.977302939495
     assert result[1] == -782.281099086326
