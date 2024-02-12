7cbac192bac7fc1aded9d685885bf0075f5a5d24
richardarsenault <richard.arsenault@etsmtl.ca>
2024-02-12 16:50:15 -0500

    fixed test with figure, fixed import problem

--- tests/optimal_interpolation/test_integration.py
+++ tests/optimal_interpolation/test_integration.py
@@ -154,4 +154,4 @@ class Test_optimal_interpolation_integration:
             self.flow_l1o_info_file,
         ]
 
-        cr.compare(self.start_date, self.end_date, files)
+        cr.compare(self.start_date, self.end_date, files, show_comparaison=False)
