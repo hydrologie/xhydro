743f3816d8aff2f30dc16f64c1419daa66af6d69
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-02-28 14:32:01 -0500

    prevent circular imports

--- xhydro/modelling/__init__.py
+++ xhydro/modelling/__init__.py
@@ -1,6 +1,11 @@
-"""The Hydrotel Hydrological Model module."""
+"""The Hydrotel Hydrological Model module.
+
+Prevents circular imports by importing in a very specific order.
+isort:skip_file
+"""
+
+from .hydrological_modelling import *
+from .calibration import *
 
 # Supported models are returned as 1st level classes
 from ._hydrotel import Hydrotel
-from .calibration import *
-from .hydrological_modelling import *
