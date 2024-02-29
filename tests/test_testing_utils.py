96bff39ed18b850dda5b06ea8743388fb72856c9
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-02-29 13:24:06 -0500

    less strict testing of version number in changelog

--- tests/test_testing_utils.py
+++ tests/test_testing_utils.py
@@ -22,13 +22,6 @@ def test_publish_release_notes(tmp_path):
     version = xh.__version__
     vsplit = version.split(".")
 
-    v_4history = (
-        vsplit[0]
-        + "."
-        + str(int(vsplit[1]) + 1 if vsplit[2] != "0" else vsplit[1])
-        + ".0"
-    )
-    assert f"## v{v_4history}" in changelog
     assert ":user:`" not in changelog
     assert ":issue:`" not in changelog
     assert ":pull:`" not in changelog
