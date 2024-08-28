2b4c23fb126eef2c59c353d114e05c0636f83ecd
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-08-28 10:30:47 -0400

    py39 compatibility

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,7 @@
 
 import os
 from pathlib import Path
+from typing import Union
 
 import pytest
 from xclim.testing.utils import open_dataset as _open_dataset
@@ -16,7 +17,7 @@ def threadsafe_data_dir(tmp_path_factory) -> Path:
 def open_dataset(threadsafe_data_dir):
     # FIXME: This is a temporary fix against the latest xclim-testdata release. It should be removed once xclim itself is updated.
     def _open_session_scoped_file(
-        file: str | os.PathLike, branch: str = "v2023.12.14", **xr_kwargs
+        file: Union[str, os.PathLike], branch: str = "v2023.12.14", **xr_kwargs
     ):
         xr_kwargs.setdefault("engine", "h5netcdf")
         return _open_dataset(
