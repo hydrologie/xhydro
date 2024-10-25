dd032541da805ef4b52e1c2c7ab38edef8989861
RondeauG <rondeau-genesse.gabriel@ouranos.ca>
2024-10-25 13:26:02 -0400

    better attributes, manage weights on time, optional delta_types

--- src/xhydro/cc.py
+++ src/xhydro/cc.py
@@ -1,9 +1,10 @@
 """Module to compute climate change statistics using xscen functions."""
 
-from typing import Optional, Union
+from datetime import datetime
 
 import numpy as np
 import xarray as xr
+import xscen as xs
 
 # Special imports from xscen
 from xscen import climatological_op, compute_deltas, ensemble_stats, produce_horizon
@@ -20,8 +21,8 @@ __all__ = [
 def sampled_indicators(
     ds: xr.Dataset,
     deltas: xr.Dataset,
-    delta_type: str,
     *,
+    delta_type: str | dict | None = None,
     ds_weights: xr.DataArray | None = None,
     delta_weights: xr.DataArray | None = None,
     n: int = 50000,
@@ -37,8 +38,10 @@ def sampled_indicators(
         The percentiles should be stored in either a dimension called "percentile" [0, 100] or "quantile" [0, 1].
     deltas : xr.Dataset
         Dataset containing the future deltas to apply to the historical indicators.
-    delta_type : str
-        Type of delta provided. Must be one of ['absolute', 'percentage'].
+    delta_type : str or dict, optional
+        Type of delta provided. Recognized values are: ['absolute', 'abs.', '+'], ['percentage', 'pct.', '%'].
+        If a dict is provided, it should map the variable names to their respective delta type.
+        If None, the variables should have a 'delta_kind' attribute.
     ds_weights : xr.DataArray, optional
         Weights to use when sampling the historical indicators, for dimensions other than 'percentile'/'quantile'.
         Dimensions not present in this Dataset, or if None, will be sampled uniformly unless they are shared with 'deltas'.
@@ -65,6 +68,9 @@ def sampled_indicators(
 
     Notes
     -----
+    Weights along the 'time' or 'horizon' dimensions are supported, but behave differently than other dimensions. They will not
+    be stacked alongside other dimensions in the new 'sample' dimension. Rather, a separate sampling will be done for each time/horizon,
+
     The future percentiles are computed as follows:
     1. Sample 'n' values from the historical distribution, weighting the percentiles by their associated coverage.
     2. Sample 'n' values from the delta distribution, using the provided weights.
@@ -119,27 +125,63 @@ def sampled_indicators(
     _, pdim, mult = _perc_or_quantile(ds)
     ds_dist = (
         _weighted_sampling(ds, percentile_weights, n=n, seed=seed)
-        .drop_vars(["sample", *percentile_weights.dims])
+        .drop_vars("sample", errors="ignore")
         .assign_coords({"sample": np.arange(n)})
     )
     deltas_dist = (
         _weighted_sampling(deltas, delta_weights, n=n, seed=seed)
-        .drop_vars(["sample", *delta_weights.dims])
+        .drop_vars("sample", errors="ignore")
         .assign_coords({"sample": np.arange(n)})
     )
 
-    # Element-wise multiplication of the ref_dist and ens_dist
-    if delta_type == "percentage":
-        fut_dist = ds_dist * (1 + deltas_dist / 100)
-    elif delta_type == "absolute":
-        fut_dist = ds_dist + deltas_dist
-    else:
-        raise ValueError(
-            f"Unknown operation '{delta_type}', expected one of ['absolute', 'percentage']."
-        )
+    # Prepare the operation to apply on the variables
+    if delta_type is None:
+        delta_type = {var: deltas[var].attrs["delta_kind"] for var in deltas.data_vars}
+    elif isinstance(delta_type, str):
+        delta_type = {var: delta_type for var in deltas.data_vars}
+
+    # Apply the deltas element-wise to the historical distribution
+    fut_dist = xr.Dataset()
+    for var in deltas.data_vars:
+        with xr.set_options(keep_attrs=True):
+            if delta_type[var] in ["absolute", "abs.", "+"]:
+                fut_dist[var] = ds_dist[var] + deltas_dist[var]
+            elif delta_type[var] in ["percentage", "pct.", "%"]:
+                fut_dist[var] = ds_dist[var] * (1 + deltas_dist[var] / 100)
+            else:
+                raise ValueError(
+                    f"Unknown operation '{delta_type[var]}', expected one of ['absolute', 'abs.', '+'], ['percentage', 'pct.', '%']."
+                )
+            fut_dist[var].attrs = ds_dist[var].attrs
+            fut_dist[var].attrs["sampling_kind"] = delta_type[var]
+            fut_dist[var].attrs["sampling_n"] = n
+            fut_dist[var].attrs["sampling_seed"] = seed
+            fut_dist[var].attrs[
+                "description"
+            ] = f"Future distribution of {fut_dist[var].attrs.get('long_name', var)} constructed from a perturbation approach and random sampling."
+            fut_dist[var].attrs[
+                "long_name"
+            ] = f"Future distribution of {fut_dist[var].attrs.get('long_name', var)}."
+            old_history = ds_dist[var].attrs.get("history", "") + deltas_dist[
+                var
+            ].attrs.get("history", "")
+            fut_dist[var].attrs["history"] = (
+                f"[{datetime.now().isoformat()}] Perturbation approach and random sampling applied to historical distribution and deltas "
+                f"using 'xhydro.sampled_indicators' and {n} samples.\n {old_history}"
+            )
+    # Build the attributes based on common attributes between the historical and delta distributions
+    fut_dist = xs.clean_up(fut_dist, common_attrs_only=[ds, deltas])
 
     # Compute future percentiles
-    fut_pct = fut_dist.quantile(ds.percentile / mult, dim="sample")
+    with xr.set_options(keep_attrs=True):
+        fut_pct = fut_dist.quantile(ds.percentile / mult, dim="sample")
+    for var in fut_pct.data_vars:
+        fut_pct[var].attrs["description"] = (
+            fut_pct[var].attrs["description"].replace("distribution", f"{pdim}s")
+        )
+        fut_pct[var].attrs["long_name"] = (
+            fut_pct[var].attrs["long_name"].replace("distribution", f"{pdim}s")
+        )
 
     if pdim == "percentile":
         fut_pct = fut_pct.rename({"quantile": "percentile"})
@@ -185,6 +227,7 @@ def _weighted_sampling(
     ds: xr.Dataset, weights: xr.DataArray, n: int = 50000, seed: int | None = None
 ) -> xr.Dataset:
     """Sample from a distribution with weights.
+    In the case of weights on a 'time' or 'horizon' dimension, the sampling is done separately for each time/horizon, but with the same seed.
 
     Parameters
     ----------
@@ -205,23 +248,51 @@ def _weighted_sampling(
     # Prepare the weights
     if weights.isnull().any():
         raise ValueError("The weights contain NaNs.")
-    weights = weights / weights.sum()  # Must equal 1
+
+    # Weights on the time dimension need to be handled differently
+    time_dim = [dim for dim in weights.dims if dim in ["time", "horizon"]]
+    if len(time_dim) > 1:
+        raise NotImplementedError(
+            "Weights on multiple time dimensions are not supported."
+        )
+    time_dim = time_dim[0] if time_dim else ""
+    other_dims = list(set(weights.dims).difference([time_dim]))
+
+    # Must equal 1
+    weights = weights / weights.sum(other_dims)
 
     # For performance reasons, remove the chunking along impacted dimensions
     ds = ds.chunk({dim: -1 for dim in weights.dims})
     weights = weights.chunk({dim: -1 for dim in weights.dims})
 
     # Stack the dimensions containing weights
-    ds = ds.stack({"sample": sorted(list(weights.dims))})
-    weights = weights.stack({"sample": sorted(list(weights.dims))})
+    ds = ds.stack({"sample": sorted(other_dims)})
+    weights = weights.stack({"sample": sorted(other_dims)})
     weights = weights.reindex_like(ds)
 
     # Sample the dimensions with weights
     rng = np.random.default_rng(seed=seed)
-    idx = rng.choice(weights.size, size=n, p=weights)
 
-    # Create the distribution dataset
-    ds_dist = ds.isel({"sample": idx}).chunk({"sample": -1})
+    if not time_dim:
+        idx = rng.choice(len(weights.sample), size=n, p=weights)
+        # Create the distribution dataset
+        ds_dist = ds.isel({"sample": idx}).chunk({"sample": -1})
+    else:
+        # 2D weights are not supported by np.random.choice, so we need to loop over the time dimension
+        ds_tmp = [
+            ds.sel({time_dim: time}).isel(
+                {
+                    "sample": rng.choice(
+                        len(weights.sample), size=n, p=weights.sel({time_dim: time})
+                    )
+                }
+            )
+            for time in ds[time_dim].values
+        ]
+        # Remove the coordinates from the sample dimension to allow concatenation
+        for i, ds_ in enumerate(ds_tmp):
+            ds_tmp[i] = ds_.reset_index("sample", drop=True)
+        ds_dist = xr.concat(ds_tmp, dim=time_dim).chunk({time_dim: -1, "sample": -1})
 
     return ds_dist
 
