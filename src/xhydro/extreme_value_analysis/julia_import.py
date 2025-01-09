4ff8b855d935bed446b35073992130a23e7483f1
ospinajulian <ospinallano.julianandres@outlook.com>
2025-01-09 15:43:36 -0500

    fixed environ variables when importern juliacall

--- src/xhydro/extreme_value_analysis/julia_import.py
+++ src/xhydro/extreme_value_analysis/julia_import.py
@@ -23,19 +23,6 @@ if "juliacall" in sys.modules:
         "Also note that xhydro will not be able to configure `PYTHON_JULIACALL_THREADS` or `PYTHON_JULIACALL_OPTLEVEL` for you."
     )
 else:
-    # Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
-    if os.environ.get("PYTHON_JULIACALL_HANDLE_SIGNALS", "yes") != "yes":
-        warnings.warn(
-            "PYTHON_JULIACALL_HANDLE_SIGNALS environment variable is set to something other than 'yes' or ''. "
-            + "You will experience segfaults if running with multithreading."
-        )
-
-    if os.environ.get("PYTHON_JULIACALL_THREADS", "auto") != "auto":
-        warnings.warn(
-            "PYTHON_JULIACALL_THREADS environment variable is set to something other than 'auto', "
-            "so xhydro was not able to set it. You may wish to set it to `'auto'` for full use "
-            "of your CPU."
-        )
 
     # TODO: Remove these when juliapkg lets you specify this
     for k, default in (
@@ -44,6 +31,20 @@ else:
         ("PYTHON_JULIACALL_OPTLEVEL", "3"),
     ):
         os.environ[k] = os.environ.get(k, default)
+
+    # Required to avoid segfaults (https://juliapy.github.io/PythonCall.jl/dev/faq/)
+    if os.environ.get("PYTHON_JULIACALL_HANDLE_SIGNALS", "no") not in ["yes", ""]:
+        warnings.warn(
+            "PYTHON_JULIACALL_HANDLE_SIGNALS environment variable is set to something other than 'yes' or ''. "
+            + "You will experience segfaults if running with multithreading."
+        )
+
+    if os.environ.get("PYTHON_JULIACALL_THREADS", "no") != "auto":
+        warnings.warn(
+            "PYTHON_JULIACALL_THREADS environment variable is set to something other than 'auto', "
+            "so xhydro was not able to set it. You may wish to set it to `'auto'` for full use "
+            "of your CPU."
+        )
 
 
 def check_function_output(func, expected_output, *args, **kwargs) -> bool:
