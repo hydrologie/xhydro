902ce77912b1ed07ecfa82d4bbf69bf9211f4bba
laugatel <laura.gatel@environnement.gouv.qc.ca>
2025-04-09 09:32:17 -0400

    test commit via anaconda prompt

--- src/xhydro/modelling/__init__.py
+++ src/xhydro/modelling/__init__.py
@@ -1,4 +1,6 @@
-"""The Hydrotel Hydrological Model module.
+# numpydoc ignore=EX01,SA01,ES01
+"""
+The Hydrotel Hydrological Model module.
 
 Prevent circular imports by importing in a very specific order.
 isort:skip_file
@@ -8,5 +10,6 @@ from .hydrological_modelling import *
 from .calibration import *
 
 # Supported models are returned as 1st level classes
+# Comment addition
 from ._hydrotel import Hydrotel
 from ._ravenpy_models import RavenpyModel
