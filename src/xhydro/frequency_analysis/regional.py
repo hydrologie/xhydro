e3966b2d4084f33953bdeb2976e7726d183baf9e
Thomas-Charles Fortier Filion <fortierfilion.thomas-charles@mdo.hydro.qc.ca>
2025-07-25 11:29:35 -0400

    ndarray typing

--- src/xhydro/frequency_analysis/regional.py
+++ src/xhydro/frequency_analysis/regional.py
@@ -524,7 +524,7 @@ def calculate_rp_from_afr(
     *,
     return_period: np.array,
     l1: xr.DataArray | None = None,
-    rp: np.array = None,
+    rp: np.ndarray | None = None,
 ) -> xr.DataArray:
     """
     Calculate return periods from Annual Flow Regime (AFR) analysis.
@@ -570,7 +570,7 @@ def calculate_return_period_from_afr(
     *,
     return_period: np.array,
     l1: xr.DataArray | None = None,
-    rp: np.array = None,
+    rp: np.ndarray | None = None,
 ) -> xr.DataArray:
     """
     Calculate return periods from Annual Flow Regime (AFR) analysis.
